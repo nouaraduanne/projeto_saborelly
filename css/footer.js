3924fb96ff2aae1f2be2bba940f3dda54529866c
Pah <pah.dsf@gmail.com>
2018-02-22 18:47:35 -0300

    Projeto Concluido

---
+++ css/footer.js
@@ -0,0 +1,12 @@
+const icone = document.querySelectorAll(".fab")
+
+function aumentaIcone(){
+    for(i = 0; i < 3; i++){
+    icone[i].style.fontSize = "50px"
+    icone[i].style.marginTop = "15px"
+    }
+}
+aumentaIcone()
+
+
+
